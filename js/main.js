101f2984d761f47e53394742a4c87673409fb8a5
Michael Mwirigi Muthamia <mwirigimichael@gmail.com>
2015-06-17 16:19:55 +0300

    added the tabs

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function() { 	
+  $('.icon_tabs .item').tab();
+
+  $('.icon_tabs').first()
+  //.sidebar('attach events', '.toggle.button')
+  ;
+
+  $('.toggle.button')
+    .removeClass('disabled')
+  ;
+  
+});
