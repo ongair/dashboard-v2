b161ad6de10cc28dad48e914898d172624be4e79
Michael Mwirigi Muthamia <mwirigimichael@gmail.com>
2015-06-29 17:12:25 +0300

    changed button colors

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ var width = $(window).width();
 $(document).ready(function() { 	
 
    $('#icon_tabs .item').tab();
+   $(".conversation").tinyscrollbar();
 
 $('.toggle_column3').click(function() {
 
