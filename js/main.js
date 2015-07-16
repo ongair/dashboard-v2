568076f7351042543a86a60c3f915dcd615935d0
Michael Mwirigi Muthamia <mwirigimichael@gmail.com>
2015-07-16 13:51:28 +0300

    changes to textfield in compose page

--- js/main.js
+++ js/main.js
@@ -12,6 +12,14 @@ $(document).ready(function() {
 	    ;
 	  })
 	;
+	$('.message .close')
+	  .on('click', function() {
+	    $(this)
+	      .closest('.message')
+	      .transition('fade')
+	    ;
+	  })
+	;
 
 	$('.compose_content .ui.dropdown')
 	  .dropdown({
